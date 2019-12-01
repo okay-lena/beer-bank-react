514aef1c4e501acce724bbcd7170baad19cf7126
lenakosyakova <kosyakova@gmail.com>
2019-12-01 11:53:04 -0500

    Fix adding/removing favorite beers

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -10,30 +10,28 @@ class Main extends Component {
     favoriteBeers: [],  // beer IDs
     beers: [
       {
-      "id": 1,
-      "starClass": "far fa-star",
-      "name": "Buzz",
-      "tagline": "A Real Bitter Experience.",
-      "first_brewed": "09/2007",
-      "description": "A light, crisp and bitter IPA brewed with English and American hops. A small batch brewed only once.",
-      "image_url": "https://images.punkapi.com/v2/keg.png",
-      "abv": 4.5,
-      "ibu": 60,
-      "ebc": 20,
-      "food_pairing": ["Spicy chicken tikka masala", "Grilled chicken quesadilla", "Caramel toffee cake"]
+        "id": 1,
+        "name": "Buzz",
+        "tagline": "A Real Bitter Experience.",
+        "first_brewed": "09/2007",
+        "description": "A light, crisp and bitter IPA brewed with English and American hops. A small batch brewed only once.",
+        "image_url": "https://images.punkapi.com/v2/keg.png",
+        "abv": 4.5,
+        "ibu": 60,
+        "ebc": 20,
+        "food_pairing": ["Spicy chicken tikka masala", "Grilled chicken quesadilla", "Caramel toffee cake"]
       },
       {
-      "id": 2,
-      "starClass": "far fa-star",
-      "name": "Trashy Blonde",
-      "tagline": "You Know You Shouldn't",
-      "first_brewed": "04/2008",
-      "description": "A titillating, neurotic, peroxide punk of a Pale Ale. Combining attitude, style, substance, and a little bit of low self esteem for good measure; what would your mother say? The seductive lure of the sassy passion fruit hop proves too much to resist. All that is even before we get onto the fact that there are no additives, preservatives, pasteurization or strings attached. All wrapped up with the customary BrewDog bite and imaginative twist.",
-      "image_url": "https://images.punkapi.com/v2/2.png",
-      "abv": 4.1,
-      "ibu": 41.5,
-      "ebc": 15,
-      "food_pairing": ["Fresh crab with lemon", "Garlic butter dipping sauce", "Goats cheese salad", "Creamy lemon bar doused in powdered sugar"]
+        "id": 2,
+        "name": "Trashy Blonde",
+        "tagline": "You Know You Shouldn't",
+        "first_brewed": "04/2008",
+        "description": "A titillating, neurotic, peroxide punk of a Pale Ale. Combining attitude, style, substance, and a little bit of low self esteem for good measure; what would your mother say? The seductive lure of the sassy passion fruit hop proves too much to resist. All that is even before we get onto the fact that there are no additives, preservatives, pasteurization or strings attached. All wrapped up with the customary BrewDog bite and imaginative twist.",
+        "image_url": "https://images.punkapi.com/v2/2.png",
+        "abv": 4.1,
+        "ibu": 41.5,
+        "ebc": 15,
+        "food_pairing": ["Fresh crab with lemon", "Garlic butter dipping sauce", "Goats cheese salad", "Creamy lemon bar doused in powdered sugar"]
       }
     ]
   }
@@ -45,16 +43,15 @@ class Main extends Component {
   onStarClickHandler = (event, beerId) => {
     if ( this.state.favoriteBeers.includes(beerId) ) {
       // remove from favoriteBeers and update state
-      let favoriteBeersToUpdate = this.state.favoriteBeers
-      favoriteBeersToUpdate.splice(favoriteBeersToUpdate.indexOf(beerId), 1)
+      this.state.favoriteBeers.splice(this.state.favoriteBeers.indexOf(beerId), 1)
       this.setState({
-          favoriteBeers: favoriteBeersToUpdate
+          favoriteBeers: this.state.favoriteBeers
       })
     } else {
       // add beerId to update favoriteBeers and state
-      let favoriteBeersToUpdate = this.state.favoriteBeers.push(beerId)
+      this.state.favoriteBeers.push(beerId)
       this.setState({
-          favoriteBeers: favoriteBeersToUpdate
+          favoriteBeers: this.state.favoriteBeers
       })
     }
     event.stopPropagation()
