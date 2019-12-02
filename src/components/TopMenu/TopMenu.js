4c3529b46b610b6f19660e9dfa3e133db2c08dcf
lenakosyakova <kosyakova@gmail.com>
2019-12-02 11:32:51 -0500

    Added sticky top menu

--- src/components/TopMenu/TopMenu.js
+++ src/components/TopMenu/TopMenu.js
@@ -4,8 +4,8 @@ import classes from './TopMenu.module.css'
 const TopMenu = () => {
   return (
   <div className={classes.TopMenu}>
-    <a href="#">Home</a>
-    <a href="#">Favorites</a>
+    <a href="#Home">Home</a>
+    <a href="#Favorites">Favorites</a>
   </div>
 )}
 
