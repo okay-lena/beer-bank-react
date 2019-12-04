b983c99b064e1b405a757725cadcd58749ff4475
lenakosyakova <kosyakova@gmail.com>
2019-12-04 14:09:06 -0500

    Fetching beers from API

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -3,73 +3,31 @@ import classes from './Main.module.css'
 import TopMenu from '../../components/TopMenu/TopMenu'
 import BeersList from '../../components/BeersList/BeersList'
 import BeerModal from '../../components/BeerModal/BeerModal'
+import axios from 'axios'
+import Loader from '../../components/Loader/Loader'
+
 
 class Main extends Component {
   state = {
     showFavorites: false,
+    beersAreLoading: true,
     favoriteBeers: [],  // beer IDs
-    beers: [
-      {
-        "id": 1,
-        "name": "Buzz",
-        "tagline": "A Real Bitter Experience.",
-        "first_brewed": "09/2007",
-        "description": "A light, crisp and bitter IPA brewed with English and American hops. A small batch brewed only once.",
-        "image_url": "https://images.punkapi.com/v2/keg.png",
-        "abv": 4.5,
-        "ibu": 60,
-        "ebc": 20,
-        "food_pairing": [
-          "Spicy chicken tikka masala",
-          "Grilled chicken quesadilla",
-          "Caramel toffee cake"
-        ]
-      },
-      {
-        "id": 2,
-        "name": "Trashy Blonde",
-        "tagline": "You Know You Shouldn't",
-        "first_brewed": "04/2008",
-        "description": "A titillating, neurotic, peroxide punk of a Pale Ale. Combining attitude, style, substance, and a little bit of low self esteem for good measure; what would your mother say? The seductive lure of the sassy passion fruit hop proves too much to resist. All that is even before we get onto the fact that there are no additives, preservatives, pasteurization or strings attached. All wrapped up with the customary BrewDog bite and imaginative twist.",
-        "image_url": "https://images.punkapi.com/v2/2.png",
-        "abv": 4.1,
-        "ibu": 41.5,
-        "ebc": 15,
-        "food_pairing": [
-          "Fresh crab with lemon",
-          "Garlic butter dipping sauce",
-          "Goats cheese salad",
-          "Creamy lemon bar doused in powdered sugar"
-        ]
-      },
-      {
-        "id" : 3,
-        "name" : "Berliner Weisse With Yuzu - B-Sides",
-        "tagline" : "Japanese Citrus Berliner Weisse.",
-        "first_brewed" : "11/2015",
-        "description" : "Japanese citrus fruit intensifies the sour nature of this German classic.",
-        "image_url" : "https://images.punkapi.com/v2/keg.png",
-        "abv" : 4.2,
-        "ibu" : 8,
-        "ebc" : 8,
-        "food_pairing" : [ "Smoked chicken wings", "Miso ramen", "Yuzu cheesecake" ]
-      },
-      {
-        "id": 4,
-        "name": "Pilsen Lager",
-        "tagline": "Unleash the Yeast Series.",
-        "first_brewed": "09/2013",
-        "description": "Our Unleash the Yeast series was an epic experiment into the differences in aroma and flavour provided by switching up your yeast. We brewed up a wort with a light caramel note and some toasty biscuit flavour, and hopped it with Amarillo and Centennial for a citrusy bitterness. Everything else is down to the yeast. Pilsner yeast ferments with no fruity esters or spicy phenols, although it can add a hint of butterscotch.",
-        "image_url": "https://images.punkapi.com/v2/4.png",
-        "abv": 6.3,
-        "ibu": 55,
-        "ebc": 30,
-        "food_pairing": [ "Spicy crab cakes", "Spicy cucumber and carrot Thai salad", "Sweet filled dumplings" ]
-      }
-    ],
+    beers: [],
     modalWindowState : {
       currentBeerId: 1,
       isShown: false
+    }
+  }
+
+  async componentDidMount() {
+    try {
+      const response = await axios.get('https://api.punkapi.com/v2/beers')
+      this.setState({
+        beers: response.data,
+        beersAreLoading: false
+      })
+    } catch (e) {
+      console.log(e)
     }
   }
 
@@ -91,12 +49,6 @@ class Main extends Component {
     })
   }
 
-  toggleModalWindow = () => {
-    if (this.state.modalWindowState.isShown) {
-      this.hideModalWindow()
-    }
-  }
-
   onStarClickHandler = (event, beerId) => {
     if ( this.state.favoriteBeers.includes(beerId) ) {
       // remove from favoriteBeers and update state
@@ -116,30 +68,35 @@ class Main extends Component {
 
   render() {
     return (
-      <div className = {classes.Main} onClick={this.toggleModalWindow}>
+      <div className = {classes.Main}>
         <TopMenu />
         <header>
           <h1>The Beer Bank</h1>
           <p>Find your favorite beer here</p>
+          />
         </header>
-        <div className= {classes.container}>
-          <BeersList
-            beers = {this.state.beers}
-            favoriteBeers = {this.state.favoriteBeers}
-            onStarClick = {this.onStarClickHandler}
-            showModalWindow = {this.showModalWindow}
-            hideModalWindow = {this.hideModalWindow}
-          />
-        <BeerModal
-          beerId = {this.state.modalWindowState.currentBeerId}
-          favoriteBeers = {this.state.favoriteBeers}
-          onStarClick = {this.state.onStarClick}
-          beers = {this.state.beers}
-          isShown = {this.state.modalWindowState.isShown}
-          showModalWindow = {this.showModalWindow}
-          hideModalWindow = {this.hideModalWindow}
-          />
-        </div>
+        { this.state.beersAreLoading
+          ? <Loader />
+          :
+          <div className= {classes.container}>
+            <BeersList
+              beers = {this.state.beers}
+              favoriteBeers = {this.state.favoriteBeers}
+              onStarClick = {this.onStarClickHandler}
+              showModalWindow = {this.showModalWindow}
+              hideModalWindow = {this.hideModalWindow}
+              />
+            <BeerModal
+              beerId = {this.state.modalWindowState.currentBeerId}
+              favoriteBeers = {this.state.favoriteBeers}
+              onStarClick = {this.state.onStarClick}
+              beers = {this.state.beers}
+              isShown = {this.state.modalWindowState.isShown}
+              showModalWindow = {this.showModalWindow}
+              hideModalWindow = {this.hideModalWindow}
+              />
+          </div>
+        }
       </div>
     )
   }
